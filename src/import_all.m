097c68c0c40a9863dae1dbb558ece5fc33a6d137
Maxime G <maxime.gardoni@epfl.ch>
2021-05-25 10:19:08 +0200

    script to import all the csv at once

---
+++ src/import_all.m
@@ -0,0 +1,53 @@
+%% YOUR BASE FOLDER
+allSubFolders = genpath('../media');
+
+
+
+
+% Parse into a cell array.
+remain = allSubFolders;
+listOfFolderNames = {};
+while true
+	[singleSubFolder, remain] = strtok(remain, ';');
+	if isempty(singleSubFolder)
+		break;
+	end
+	listOfFolderNames = [listOfFolderNames singleSubFolder];
+end
+numberOfFolders = length(listOfFolderNames)
+
+for k = 1 : numberOfFolders
+	% Get this folder and print it out.
+	thisFolder = listOfFolderNames{k};
+	fprintf('Processing folder %s\n', thisFolder);
+	
+	% Get the csv files
+	filePattern = sprintf('%s/*.csv', thisFolder);
+	baseFileNames = dir(filePattern);
+
+	numberOfImageFiles = length(baseFileNames);
+	if numberOfImageFiles >= 1
+		% Go through all those files.
+		for f = 1 : numberOfImageFiles
+			fullFileName = fullfile(thisFolder, baseFileNames(f).name);
+			fprintf('     Processing file %s\n', fullFileName);
+            
+            variables = hri_load_logfile("../media/biceps_haut/force_forte_coude_int.csv");
+            emg_signal = variables.emg_signal;
+            emg_signal = emg_signal - mean(emg_signal);
+            position = variables.paddle_pos__deg_;
+            threshold = 0;
+            bin_array= position > threshold;
+            
+            %_____________________TODO_______________________________ :
+%             put the variables (emg_signal, bin_array) in some data
+%             structure with the label baseFileNames(f).name
+%               maybe we can use table, or struct, i'm not sure
+            
+		end
+	else
+		fprintf('     Folder %s has no files in it.\n', thisFolder);
+	end
+end
+
+
